d9b9d3c3b00179a35f2142a40d521ecc764a0d17
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2018-10-18 15:41:30 -0700

    small changes

--- gif-search/app.js
+++ gif-search/app.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var app = express();
+var exphbs = require('express-handlebars');
 
 // INITIALIZE THE GIPHY-API LIBRARY
 var giphy = require('giphy-api')();
 app.get('/', function (req, res) {
-  let defaultSearch = req.query.term || "puppy"
+  let defaultSearch = req.query.term || "trending"
   giphy.search(defaultSearch, function (err, response) {
     res.render('home', {gifs: response.data})
   });
 });
 
-app.listen(3000, function () {
-  console.log('Gif Search listening on port localhost:3000!');
-});
-
-var exphbs  = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+app.listen(3000, function () {
+  console.log('Gif Search listening on port localhost:3000!');
+});
